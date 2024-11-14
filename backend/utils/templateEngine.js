c07dfc983e53581f3ed36f2e4d201cb6358f870e
David Arua <brute@Davids-Laptop.local>
2024-11-14 12:38:26 +0200

    files aranged

--- backend/utils/templateEngine.js
+++
@@ -1,8 +0,0 @@
-export const renderTemplate = (templateContent, context) => {
-  let renderedContent = templateContent;
-  for (const [key, value] of Object.entries(context)) {
-    const placeholder = new RegExp(`{{${key}}}`, "g"); // Matches all instances of {{key}}
-    renderedContent = renderedContent.replace(placeholder, value);
-  }
-  return renderedContent;
-};
